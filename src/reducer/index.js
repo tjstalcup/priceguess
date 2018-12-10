4172ed2031f9ac1ddd1de2c260e096f0114bc230
Justin  Newby <jnewby91@gmail.com>
2018-12-10 18:52:53 -0500

    Commented out code that helps with creating initial state

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,14 @@
-const initialState= {
- 
+const initialState= {   
+
+
 
 }; 
+
+
+// const defaultState = {
+//     score: 0,
+//     question: 1,
+//     currentPriceOptions: [],
+//     playerGuesses: [{questionNumber: 1, actualPrice: '1.11', guessedPrice: '2.22', correct: false, }],
+//     actualProductHistory: [{itemName:'',productPicture:'url',itemDescription:''}],
+// }
