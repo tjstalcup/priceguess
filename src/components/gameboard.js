902efc82b4f475c2a180a8fcd203ab8a518f7f25
Justin  Newby <jnewby91@gmail.com>
2018-12-18 17:50:41 -0500

    Adds loading statement to gameboard.js

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,49 +1,69 @@
-import React from 'react'; 
-import {connect} from 'react-redux'; 
-import './gameboard.css'; 
-import ItemImage from './itemImage'; 
+import React from 'react';
+import { connect } from 'react-redux';
+import './gameboard.css';
+import ItemImage from './itemImage';
 import ItemDescription from './itemDescription';
 import Scoreboard from './scoreboard';
 import PriceChoice from './priceChoice';
-import {fetchItemInfo} from '../actions/bestBuyAction';
+import { fetchItemInfo } from '../actions/bestBuyAction';
 import { fetchItemInfoRequest, fetchItemInfoSuccess } from '../actions/bestBuyAction';
 
 
 export class GamePage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
     // Make constructor for the dispatch
-     componentDidMount(){
-         this.props.dispatch(fetchItemInfo())
-     }
-    
+    componentDidMount() {
+        this.props.dispatch(fetchItemInfo())
+    }
+
     render() {
-    return(
-        <div className="GamePage">
-            <h1>GameBoard JS</h1>
-            <Scoreboard 
-            timer={10}
-            />
-{/*             <ItemImage />
- */}            <ItemDescription />
-           {/*  <PriceChoice /> */} 
-      </div>
- 
-    )   
-}
+        if (this.props.loading === true){
+            return <h1>LOADING</h1>
+        }
+            
+        
+        
+        {/* 
+            1. If loading or error state of the bestBuyCall is true, 
+            then render don't render the ItemDescription or PriceChoice
+
+            Possibly a spinner should go right here while loading
+
+            2.After bestBuyCall.state.actualProducts has informationn in the state,
+            the props can be mapped to Item Description adn Price Component 
+        
+        */}
+
+        return (
+            <div className="GamePage">
+                <h1>GameBoard JS</h1>
+                <Scoreboard
+                    timer={10}
+                />
+                {/*             <ItemImage />
+ */}            {/* <ItemDescription /> */}
+                {/*  <PriceChoice /> */}
+            </div>
+
+        )
+    }
 }
 
 
 //Finish setting up dispatch for the class
 
-const mapStatetoProps = (state) => 
-{ console.log(state)
-    return state;
+const mapStatetoProps = (state) => {
+    console.log(state)
+    return ({
+        loading: state.bestBuyCall.loading
+        
+    })
     // ({
     //     itemDescription: state.bestBuyCall.actualProductHistory[state.bestBuyCall.actualProductHistory.length - 1].itemDescription
     // })
-    
+
 }
 
 export default connect(mapStatetoProps)(GamePage); 
